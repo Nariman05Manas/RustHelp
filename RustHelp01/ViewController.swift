d240de5f9d9ad0948a1edc71b45b3355510c134d
Nariman05Manas <89285569904@mail.ru>
2024-04-10 00:15:24 +0300

    Initial Commit

---
+++ RustHelp01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RustHelp01
+//
+//  Created by qwerty on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
